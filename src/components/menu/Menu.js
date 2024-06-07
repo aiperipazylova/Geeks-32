6e85642af41e1b7fa7b3150a02693c343f418aab
Айпери Пазылова <aiperipazylova@Ajperis-MacBook-Pro.local>
2024-06-07 10:17:09 +0600

    geeks-32

---
+++ src/components/menu/Menu.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const Menu = () => {
+    return (
+        <ul>
+        
+                <Link to='/pokemon'>
+                    pokemon
+                </Link>
+        
+        </ul>
+    );
+};
+
+export default Menu;
